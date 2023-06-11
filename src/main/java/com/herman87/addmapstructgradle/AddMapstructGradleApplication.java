8868061d120b72701a85ca302faaef3e0280130b
Essoungou Herman <hermanessoungou@gmail.com>
2023-06-11 23:18:10 +0100

    I build the project, I add all dependencies about lombok and mapstruct

---
+++ src/main/java/com/herman87/addmapstructgradle/AddMapstructGradleApplication.java
@@ -0,0 +1,13 @@
+package com.herman87.addmapstructgradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddMapstructGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddMapstructGradleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/herman87/addmapstructgradle/AddMapstructGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.herman87.addmapstructgradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddMapstructGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
